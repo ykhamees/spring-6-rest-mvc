fdec0ed200645ae4013acdfd953ead696416548f
Yehia Khamis <yehia.khamees@gmail.com>
2024-11-15 09:49:19 +0300

    Exercise completed

--- src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(NotFoundException.class)
